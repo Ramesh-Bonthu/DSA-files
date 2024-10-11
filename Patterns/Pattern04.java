3ed8007a808d8040ba1720c657b3ec02aeb4d4cb
Ramesh-Bonthu <rameshbonthu77@gmail.com>
2024-10-11 06:55:47 +0530

    modified

--- Patterns/Pattern04.java
+++ Patterns/Pattern04.java
@@ -1,16 +1,43 @@
 package Patterns;
 
-/*
- *        *
- *       ***
- *      *****
- *     *******
- *    *********   
- */
-
 public class Pattern04 {
   public static void main(String[] args) {
+    Pattern04 obj = new Pattern04();
+    obj.triangle();
+    System.out.println();
+    obj.revTriangle();
+  }
+
+  /*
+   * *
+   * ***
+   * *****
+   * *******
+   * *********
+   */
+
+  void triangle() {
     for (int i = 1; i <= 5; i++) {
+      for (int s = 5; s >= i; s--) {
+        System.out.print(" ");
+      }
+      for (int j = 1; j <= 2 * i - 1; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+  /*
+   * *********
+   * *******
+   * *****
+   * ***
+   * *
+   */
+
+  void revTriangle() {
+    for (int i = 5; i >= 1; i--) {
       for (int s = 5; s >= i; s--) {
         System.out.print(" ");
       }
